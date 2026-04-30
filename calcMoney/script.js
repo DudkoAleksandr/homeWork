326e808c401956b1d743f3413724576d753c1d60
Sanya9700 <sanya25051991@gmail.com>
2026-04-30 08:59:28 +0400

    loader add

--- calcMoney/script.js
+++ calcMoney/script.js
@@ -3,6 +3,7 @@ const currencyIn = document.querySelector(".currency-in");
 const inputCurrency = document.querySelector(".input__currency");
 const inputResualt = document.querySelector(".input__resualt");
 const btnResualt = document.querySelector(".btn__resualt");
+const loader = document.querySelector(".loader");
 
 // btnResualt.addEventListener('click', async () => {
 //     const currencyAll = `https://api.api-ninjas.com/v1/convertcurrency?have=${currency.value}&want=${currencyIn.value}&amount=${inputCurrency.value}`;
@@ -19,13 +20,18 @@ const btnResualt = document.querySelector(".btn__resualt");
 btnResualt.addEventListener("click", () => {
   const currencyAll = `https://api.api-ninjas.com/v1/convertcurrency?have=${currency.value}&want=${currencyIn.value}&amount=${inputCurrency.value}`;
 
-fetch(currencyAll, {
-          headers: {
-            "X-Api-Key": "pMjMw1q0V53zSAtU9SAKlgs8BF3eGqrWrhvjLqn5",
-          },
-  }).then((info) => {
-    return info.json()
-  }).then((resualt) => {
-    inputResualt.value = resualt.new_amount;
+  loader.classList.remove("hidden");
+
+  fetch(currencyAll, {
+    headers: {
+      "X-Api-Key": "pMjMw1q0V53zSAtU9SAKlgs8BF3eGqrWrhvjLqn5",
+    },
   })
+    .then((info) => {
+      return info.json();
+    })
+    .then((resualt) => {
+      inputResualt.value = resualt.new_amount;
+      loader.classList.add("hidden");
+    });
 });
