59e157ce3068020eff3201c31e1e46ab720a5edf
Sanya9700 <sanya25051991@gmail.com>
2026-05-07 09:24:29 +0400

    date

---
+++ timers/script.js
@@ -0,0 +1,27 @@
+// console.log('start')
+// setTimeout(() => {
+//     console.log(1+1)
+// }, 0)
+// console.log('and')
+
+// setInterval(() => {
+//     console.log(1)
+// }, 3000)
+
+const msDateNew = new Date()
+console.log(msDateNew)
+
+// const msDate = Date()
+// console.log(msDate)
+
+const hours = msDateNew.getHours()
+console.log(hours)
+const minuts = msDateNew.getMinutes()
+console.log(minuts)
+const seconds = msDateNew.getSeconds()
+console.log(seconds)
+
+const date = msDateNew.toDateString()
+console.log(date)
+const local = msDateNew.toLocaleDateString()
+console.log(local)
